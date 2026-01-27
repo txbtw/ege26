0eb8bb3d19dd4483b796e7a4e70654f9327cb900
марк <wizor4k@gmail.com>
2026-01-27 13:19:09 +0200

    не стоят рядом одинаковые

--- lesson/kkjg.py
+++ lesson/kkjg.py
@@ -5,6 +5,6 @@ from string import *
 cnt = 0
 for val in product(printable[:16], repeat=4):
     val = ''.join(val)
-    if val[0] != '0' and val.count('3') == 1 and (i + i not in val for i in printable[:16]):
+    if val[0] != '0' and val.count('3') == 1 and all(i + i not in val for i in printable[:16]):
         cnt += 1
 print(cnt)
